47b9216b72a108548be02074de6fd3950863fdd5
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-05-26 15:14:52 +0000

    CAD-2907 workbench block-propagation analysis:  fast re-iteration mode

--- nix/workbench/analyse.sh
+++ nix/workbench/analyse.sh
@@ -1,20 +1,32 @@
 usage_analyse() {
      usage "analyse" "Analyse cluster runs" <<EOF
     block-propagation RUN-NAME
-                          Block propagation analysis for the entire cluster
+                          Block propagation analysis for the entire cluster.
 
     machine-timeline RUN-NAME MACH-NAME
                           Produce a general performance timeline for MACH-NAME
 
+    Options of 'analyse' command:
+
+       --reanalyse        Skip the preparatory steps and launch 'locli' directly
+       --time             Time the 'locli' executable runs
 EOF
 }
 
 analyse() {
+local skip_preparation= time=
+while test $# -gt 0
+do case "$1" in
+       --reanalyse | --re ) skip_preparation='true';;
+       --time )             time='eval time';;
+       * ) break;; esac; shift; done
+
 local op=${1:-$(usage_analyse)}; shift
 
 case "$op" in
     block-propagation | bp )
         local usage="USAGE: wb analyse $op [RUN-NAME=current]"
+
         local name=${1:-current}
         local dir=$(run get "$name")
         local adir=$dir/analysis
@@ -33,23 +45,27 @@ TraceBlockFetchServerSendBlock
 TraceDownloadedHeader
 CompletedBlockFetch
 EOF
+
         ## 1. enumerate logs, filter by keyfile & consolidate
         local logdirs=("$dir"/node-*/)
 
-        msg "filtering logs in: $dir/node-* "
-        local jq_args=(
-            --sort-keys
-            --compact-output
-            $(wb backend lostream-fixup-jqargs "$dir")
-            ' delpaths([["app"],["env"],["loc"],["msg"],["ns"],["sev"]])
-            '"$(wb backend lostream-fixup-jqexpr)"
-        )
-        for d in "${logdirs[@]}"
-        do ## TODO: supervisor-specific logfile layout
-           grep -hFf "$keyfile" $(ls "$d"/stdout* | tac) | jq "${jq_args[@]}" > \
+        if test -z "$skip_preparation" -a -n "$(ls "$adir"/logs-node-*.flt.json)"
+        then
+            msg "filtering logs in: $dir/node-* "
+            local jq_args=(
+                --sort-keys
+                --compact-output
+                $(wb backend lostream-fixup-jqargs "$dir")
+                ' delpaths([["app"],["env"],["loc"],["msg"],["ns"],["sev"]])
+                '"$(wb backend lostream-fixup-jqexpr)"
+            )
+            for d in "${logdirs[@]}"
+            do ## TODO: supervisor-specific logfile layout
+                grep -hFf "$keyfile" $(ls "$d"/stdout* | tac) | jq "${jq_args[@]}" > \
                 "$adir"/logs-$(basename "$d").flt.json &
-        done
-        wait
+            done
+            wait
+        fi
 
         msg "log sizes:  (files: $(ls "$adir"/*.flt.json | wc -l), lines: $(cat "$adir"/*.flt.json | wc -l))"
 
@@ -62,7 +78,7 @@ EOF
             --analysis-json   "$adir"/block-event-stream.json
         )
 
-        locli 'analyse' 'block-propagation' \
+        ${time} locli 'analyse' 'block-propagation' \
             "${locli_args[@]}" "$adir"/*.flt.json;;
 
     machine-timeline | machine | mt )
@@ -80,9 +96,12 @@ EOF
 
         ## 1. enumerate logs, filter by keyfile & consolidate
         local logs=("$dir"/$mach/stdout) consolidated="$adir"/logs-$mach.json
-        grep -hFf "$keyfile" "${logs[@]}"  > "$consolidated"
-        msg "analysing logs of:  $mach  (lines: $(wc -l "$consolidated"))"
+        if test -z "$skip_preparation" -a -n "$(ls "$adir"/logs-$mach.json)"
+        then
+            grep -hFf "$keyfile" "${logs[@]}"  > "$consolidated"
+        fi
 
+        msg "analysing logs of:  $mach  (lines: $(wc -l "$consolidated"))"
         local locli_args=(
             --genesis         "$dir"/genesis/genesis.json
             --run-metafile    "$dir"/meta.json
@@ -98,7 +117,7 @@ EOF
             # --derived-vectors-1-csv   "$adir"/logs-$mach.derived.1.csv
         )
 
-        locli 'analyse' 'machine-timeline' \
+        ${time} locli 'analyse' 'machine-timeline' \
             "${locli_args[@]}" "$consolidated";;
 
     * ) usage_analyse;; esac
